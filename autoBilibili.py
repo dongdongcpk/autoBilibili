5816ac06e1b2f12ea14e830fc50dab9045574c0f
dongdonggun <138856015@qq.com>
2015-04-29 21:20:40 +0800

    add vdcode function

--- autoBilibili.py
+++ autoBilibili.py
@@ -4,29 +4,64 @@ import cookielib
 import urllib2
 import urllib
 
-baidu_url = 'http://www.baidu.com'
-cj = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-urllib2.install_opener(opener)
-resp = urllib2.urlopen(baidu_url)
-print resp.read()
-print '*' * 20
-login_url = 'https://passport.baidu.com/?login'
-para = {
-    'userName': '',
-    'password': '',
-    'mem_pass': 'on'
+headers = {
+    'Host': 'account.bilibili.com',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+    'Referer': 'https://account.bilibili.com/login',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Connection': 'keep-alive'
+}
+
+class Bilibili:
+    def __init__(self, vdcode):
+        #登录的url
+        self.login_url = 'https://account.bilibili.com/login'
+        #代理ip
+        self.proxy_url = 'http://120.193.146.97:843'
+        #头部信息
+        self.login_headers = headers
+        #手机号，密码，验证码
+        self.userid = '18698666910'
+        self.pwd = 'lnONcg/PoepsqdhLnzuLvjktP/K6rg7XDYknzp13aDRBFc+MKbuetAS27PTq8iWPIW/r8ETzjLYSitfi0Pu//g=='
+        self.vdcode = vdcode
+        
+        self.post = {
+             'act': 'login',
+             'gourl': 'http://www.bilibili.com/',
+             'keeptime': 604800,
+             'userid': self.userid,
+             'pwd': self.pwd,
+             'vdcode': self.vdcode,
         }
-post_data = urllib.urlencode(para)
-req = urllib2.Request(login_url, post_data)
-req.add_header('User-Agent', 'userAgentIE9')
-req.add_header('Content-Type', 'application/x-www-form-urlencoded')
-req.add_header('Cache-Control', 'no-cache')
-req.add_header('Accept', '*/*')
-req.add_header('Connection', 'Keep-Alive')
-resp = urllib2.urlopen(req)
-print resp.read()
+        #将post数据进行编码转换
+        self.post_data = urllib.urlencode(self.post)
+        #设置代理
+        self.proxy = urllib2.ProxyHandler({'http': self.proxy_url})
+        #设置cookie
+        self.cookie = cookielib.LWPCookieJar()
+        #设置cookie处理器
+        self.cookie_handler = urllib2.HTTPCookieProcessor(self.cookie)
+        #设置登录时用到的opener，它的open方法相当于urllib2.urlopen
+        self.opener = urllib2.build_opener(self.cookie_handler, self.proxy, urllib2.HTTPHandler)
+        
+def get_vdcode():
+    '''
+    获取验证码
+    '''
+    vdcode_url = 'https://account.bilibili.com/captcha'
+    req = urllib2.Request(vdcode_url, headers = headers)
+    resp = urllib2.urlopen(req)
+    html = resp.read()
+    f = open('./vdcode.png', 'w')
+    f.write(html)
+    f.close()
 
-
-# if __name__ == '__main__':
-#     pass
+if __name__ == '__main__':
+    get_vdcode()
+    vdcode = raw_input('请打开当前路径下名为vdcode.png的图片，输入验证码：')
+#     bili = Bilibili(vdcode)     
+        
+        
+        
+        
+        
