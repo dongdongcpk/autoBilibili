08ef0dd81ed0e4a94c6411287ead817f5dbbb56f
dongdonggun <138856015@qq.com>
2015-04-29 21:38:25 +0800

    change .gitignore

--- autoBilibili.py
+++ autoBilibili.py
@@ -3,6 +3,7 @@
 import cookielib
 import urllib2
 import urllib
+from bs4 import BeautifulSoup as bs
 
 headers = {
     'Host': 'account.bilibili.com',
@@ -22,7 +23,7 @@ class Bilibili:
         self.login_headers = headers
         #手机号，密码，验证码
         self.userid = '18698666910'
-        self.pwd = 'lnONcg/PoepsqdhLnzuLvjktP/K6rg7XDYknzp13aDRBFc+MKbuetAS27PTq8iWPIW/r8ETzjLYSitfi0Pu//g=='
+        self.pwd = ''
         self.vdcode = vdcode
         
         self.post = {
@@ -44,6 +45,12 @@ class Bilibili:
         #设置登录时用到的opener，它的open方法相当于urllib2.urlopen
         self.opener = urllib2.build_opener(self.cookie_handler, self.proxy, urllib2.HTTPHandler)
         
+    def start(self):
+        req = urllib2.Request(self.login_url, self.post_data, self.login_headers)
+        resp = self.opener.open(req)
+        html = resp.read()
+        return html
+        
 def get_vdcode():
     '''
     获取验证码
@@ -59,7 +66,10 @@ def get_vdcode():
 if __name__ == '__main__':
     get_vdcode()
     vdcode = raw_input('请打开当前路径下名为vdcode.png的图片，输入验证码：')
-#     bili = Bilibili(vdcode)     
+    bili = Bilibili(vdcode)
+    html = bili.start()
+    soup = bs(html)
+    print soup.prettify()     
         
         
         
