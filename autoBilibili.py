fffa1eb7a3af7d1355e521ac7d8af03e33684ba6
dongdonggun <138856015@qq.com>
2015-05-03 23:03:57 +0800

    change autoBilibili.py

--- autoBilibili.py
+++ autoBilibili.py
@@ -10,11 +10,13 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
     'Referer': 'https://account.bilibili.com/login',
     'Content-Type': 'application/x-www-form-urlencoded',
-    'Connection': 'keep-alive'
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0',
+    'Origin': 'https://account.bilibili.com'
 }
 
 class Bilibili:
-    def __init__(self, vdcode):
+    def __init__(self):
         #登录的url
         self.login_url = 'https://account.bilibili.com/login'
         #代理ip
@@ -24,52 +26,76 @@ class Bilibili:
         #手机号，密码，验证码
         self.userid = '18698666910'
         self.pwd = ''
-        self.vdcode = vdcode
+        self.vdcode = ''
         
-        self.post = {
-             'act': 'login',
-             'gourl': 'http://www.bilibili.com/',
-             'keeptime': 604800,
-             'userid': self.userid,
-             'pwd': self.pwd,
-             'vdcode': self.vdcode,
-        }
-        #将post数据进行编码转换
-        self.post_data = urllib.urlencode(self.post)
         #设置代理
         self.proxy = urllib2.ProxyHandler({'http': self.proxy_url})
         #设置cookie
-        self.cookie = cookielib.LWPCookieJar()
+        self.cookie = cookielib.CookieJar()
         #设置cookie处理器
         self.cookie_handler = urllib2.HTTPCookieProcessor(self.cookie)
         #设置登录时用到的opener，它的open方法相当于urllib2.urlopen
         self.opener = urllib2.build_opener(self.cookie_handler, self.proxy, urllib2.HTTPHandler)
         
-    def start(self):
+    def post(self, vdcode):
+        '''
+        发送登录post请求
+        '''
+        self.vdcode = vdcode
+        self.post = {
+            'act': 'login',
+            'gourl': 'https://account.bilibili.com/login/dologin',
+            'keeptime': 604800,
+            'userid': self.userid,
+            'pwd': self.pwd,
+            'vdcode': self.vdcode,
+        }
+        #将post数据进行编码转换
+        self.post_data = urllib.urlencode(self.post)
         req = urllib2.Request(self.login_url, self.post_data, self.login_headers)
         resp = self.opener.open(req)
         html = resp.read()
         return html
+    
+    def open(self):
+        '''
+        打开login页面
+        '''
+        req = urllib2.Request(self.login_url, headers = self.login_headers)
+        resp = self.opener.open(req)
+        return resp
         
-def get_vdcode():
-    '''
-    获取验证码
-    '''
-    vdcode_url = 'https://account.bilibili.com/captcha'
-    req = urllib2.Request(vdcode_url, headers = headers)
-    resp = urllib2.urlopen(req)
-    html = resp.read()
-    f = open('./vdcode.png', 'w')
-    f.write(html)
-    f.close()
+    def get_vdcode(self):
+        '''
+        获取验证码
+        '''
+        vdcode_url = 'https://account.bilibili.com/captcha'
+        req = urllib2.Request(vdcode_url, headers = headers)
+        resp = self.opener.open(req)
+        html = resp.read()
+        f = open('./vdcode.png', 'w')
+        f.write(html)
+        f.close()
 
 if __name__ == '__main__':
-    get_vdcode()
+    bili = Bilibili()
+    bili.open()
+    for i in bili.cookie:
+        headers['Cookie'] = i.name + '=' + i.value
+    bili.get_vdcode()
     vdcode = raw_input('请打开当前路径下名为vdcode.png的图片，输入验证码：')
-    bili = Bilibili(vdcode)
-    html = bili.start()
+    html = bili.post(vdcode)
     soup = bs(html)
-    print soup.prettify()     
+    print soup.prettify()
+    
+    
+    
+    
+    
+    
+    
+    
+    
         
         
         
