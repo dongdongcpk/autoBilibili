392a2306c23aa7ea9b362fac2f34b695dac9726d
dongdonggun <138856015@qq.com>
2015-05-11 17:57:45 +0800

    delete unused code

--- baiduSearch.py
+++ baiduSearch.py
@@ -4,27 +4,6 @@ import urllib2 as url
 import urllib
 from bs4 import BeautifulSoup as bs
 
-# def baidu_search(keyword):
-#     p = {'wd': keyword}
-#     res = url.urlopen("http://www.baidu.com/s?" + urllib.urlencode(p))
-#     html = res.read()
-#     return html
-
-# html = baidu_search('静夜思的作者是谁')
-# soup = bs(html)
-# # print soup.get_text()
-# # print soup.prettify()
-# author_list = ['杜甫', '李清照', '王维', '李白']
-# author_dict = {}
-# for i in author_list:
-#     count = len(soup.find_all(text = i))
-#     author_dict[i] = count
-    
-# print author_dict
-# sorted_author_list = sorted(author_dict, key = lambda x : x[1], reverse = True)
-# print sorted_author_list
-# print '作者是：' + sorted_author_list[0]
-
 class Search():
 	def baidu_search(self, keyword):
 	    p = {'wd': keyword}
